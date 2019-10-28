10ef7ebe8f12f7a8fd08cf05a9dc4a0d09b5d2f6
John Lombard <john.lombard1996@gmail.com>
2019-10-28 11:20:08 -0400

    invalid search working and color added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import API from './utils/API';
 import Card from './Card'
 
 // If 404 is returned display Please enter valid ticker
+// Clear state
+// Show please enter valid ticker
+
+
 // loader
 // colors of green and red negative
 // css
@@ -11,13 +15,14 @@ import Card from './Card'
 class App extends React.Component {
   state = {
     result: {},
-    search: ""
+    search: "", 
+    err: null
   };
 
   searchStock = query => {
     API.search(query)
       .then(res => this.setState({ result: res.data }))
-      .catch(err => console.log(err));
+      .catch(err => this.setState({ err }));
   };
 
   handleInputChange = event => {
@@ -30,6 +35,7 @@ class App extends React.Component {
   handleFormSubmit = event => {
     event.preventDefault();
     this.searchStock(this.state.search);
+    this.setState({ err: null})
   };
     
     render() {
@@ -41,7 +47,7 @@ class App extends React.Component {
                   handleInputChange={this.handleInputChange}
                   handleFormSubmit={this.handleFormSubmit}
                 />
-                <Card stockInfo={this.state.result}/>
+                <Card err={this.state.err} stockInfo={this.state.result}/>
                
             </div>
         )
--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,49 @@
 import React from 'react';
 
 
-const Card = ({stockInfo}) => {
-    let {companyName, 
-        symbol, 
-        primaryExchange, 
-        latestPrice, 
-        week52High, 
-        week52Low, 
+const Card = ({ stockInfo, err }) => {
+    let { companyName,
+        symbol,
+        primaryExchange,
+        latestPrice,
+        week52High,
+        week52Low,
         peRatio,
         change,
         changePercent,
         ytdChange
-    } = stockInfo 
-    
+    } = stockInfo
+    let percentChange;
+
+    if (change > 0) {
+        percentChange = 'green'
+    } else if (change < 0) {
+        percentChange = 'red'
+    } else if (!err === null) {
+        percentChange = null
+    }
+
     return (
-        <div className="">
-            <h1>Company Name: {companyName}</h1> 
-            <h3>Symbol: {symbol}</h3>
-            <h3>Primary Exchange: {primaryExchange}</h3>
-            <h3>Latest Price: {latestPrice}</h3>
-            <h3>52 Week High: {week52High}</h3>
-            <h3>52 Week Low: {week52Low}</h3>
-            <h3>PE Ratio: {peRatio}</h3>
-            <h3>Price Change: {change}</h3>
-            <h3>Percent Change: {changePercent}</h3>
-            <h3>YTD Change:{ytdChange}</h3> 
-        </div> 
+
+        <div style={{ backgroundColor: percentChange }} className="">
+            {console.log(err)}
+            {err === null &&
+                <div>
+                    <h1>Company Name: {companyName}</h1>
+                    <h3>Symbol: {symbol}</h3>
+                    <h3>Primary Exchange: {primaryExchange}</h3>
+                    <h3>Latest Price: {latestPrice}</h3>
+                    <h3>52 Week High: {week52High}</h3>
+                    <h3>52 Week Low: {week52Low}</h3>
+                    <h3>PE Ratio: {peRatio}</h3>
+                    <h3 >Price Change: {change}</h3>
+                    <h3>Percent Change: {changePercent}</h3>
+                    <h3>YTD Change:{ytdChange}</h3>
+                </div>
+             || <div >Please Enter Valid Ticker</div>}
+        </div>
     );
+
 }
 
 export default Card;
